cbe99f12d6c56bd714b884e9a899736cd4f9e40a
Marco Martins <a61243@alunos.ipb.pt>
2025-07-23 00:50:12 +0100

    primeiro contacto criado um Button

--- journaling/app/index.tsx
+++ journaling/app/index.tsx
@@ -1,15 +1,18 @@
-import { Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={{ marginBottom: 20 }}>Primeiro teste de react native</Text>
+      <Button title="Press me" onPress={() => alert("Button pressed!")} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
